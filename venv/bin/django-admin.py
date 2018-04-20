ba080bf5a324f681b7b9b1effebfc050d491b5ef
Shashank <skmurthy85@gmail.com>
2018-04-20 12:52:31 +0530

    Adding all files

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/shash/Learning/CI-CD/Ansible-docker-Pluralsight/da/todobackend/venv/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
